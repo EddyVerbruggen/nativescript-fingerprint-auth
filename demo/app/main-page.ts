0c4be416e4f1478041301c1162d7e10ce4e18079
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-06-25 19:41:42 +0200

    linting

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -5,6 +5,6 @@ import { HelloWorldModel } from "./main-view-model";
 // Event handler for Page 'loaded' event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
   // Get the event sender
-  let page = <pages.Page>args.object;
+  const page = <pages.Page>args.object;
   page.bindingContext = new HelloWorldModel();
 }
